f62c514eaea646caab153a215238a5004c54784f
Moammad Farhad <mfarhadattari>
2023-03-16 11:01:46 +0600

    debug show data from local storage

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ startQuiz.addEventListener("click", () => {
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
   const data = await res.json();
+  quizData = data ;
   displayQuiz(data);
 };
 
@@ -58,7 +59,6 @@ const displayQuiz = (data) => {
   }
   
   data.forEach((quiz, i) => {
-    console.log(quiz, i)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -108,7 +108,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
   if (storage) {
     localStorage.setItem(
       "results",
@@ -153,9 +153,9 @@ document.querySelector("#submit").addEventListener("click", () => {
   </div>
   
   <button onclick="location.reload();" class="bg-green-600 text-white w-full py-2 rounded mt-16">Restart</button>
-  ${
-    storage
-      ? `<div class="mt-5">
+  ${storage
+      ? `
+      <div class="mt-5">
       <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
     <div
     class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
